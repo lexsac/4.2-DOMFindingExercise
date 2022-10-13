e81ab8a54179263728da60fbcd10f53d9119433b
Lexsa Campbell <lexsacampbell@gmail.com>
2022-10-13 15:19:07 -0700

    Upload DOM Finding Exercise

---
+++ script.js
@@ -0,0 +1,53 @@
+// Write the code necessary to do the following:
+
+// Select the section with an id of container without using querySelector.
+document.getElementById('#container');
+
+// Select the section with an id of container using querySelector.
+document.querySelector('#container');
+
+// Select all of the list items with a class of “second”.
+document.querySelectorAll('.second');
+
+//OR
+
+document.getElementsByClassName('.second');
+
+// Select a list item with a class of third, but only the list item inside of the ol tag.
+document.querySelector('ol .third');
+
+// Give the section with an id of container the text “Hello!”.
+const container = document.getElementById('#container');
+container.innerText = 'Hello!';
+
+// Add the class main to the div with a class of footer.
+const footer = document.querySelector('.footer');
+footer.classList.add('main');
+
+// Remove the class main on the div with a class of footer.
+const footer = document.querySelector('.footer');
+footer.classList.remove('main');
+
+// Create a new li element.
+const newLi = document.createElement('li');
+
+// Give the li the text “four”.
+newLi.innerText = 'four';
+
+// Append the li to the ul element.
+const unorderedList = document.querySelector(ul);
+unorderedList.appendChild(newLi);
+
+// Loop over all of the lis inside the ol tag and give them a background color of “green”.
+let liInsideOl = document.querySelectorAll("ol li");
+
+for(let i = 0; i < liInsideOl.length; i++){
+    liInsideOl[i].style.backgroundColor = "green";
+}
+
+// Remove the div with a class of footer
+let footer = document.querySelector('.footer');
+footer.remove();
+
+
+
